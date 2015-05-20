1b922f77e95b1160cd8d52c1dea3cc75b1ee24bd
Branden Ghena <brghena@umich.edu>
2015-05-20 15:04:21 -0400

    Demo colorPage accessor from workshop
    (first rev. No idea at all if this works)

---
+++ accessors/lighting/colorPage.js
@@ -0,0 +1,24 @@
+// name:   Color Page
+// author: Branden Ghena
+// email: brghena@umich.edu
+
+/* Color control for a webpage
+ * ======================
+ *
+ * memristor-v1.eecs.umich.edu
+ */
+
+var post_url;
+
+function* init () {
+    provide_interface('/lighting/light', {
+        'onoff/Power': Power
+    });
+
+    post_url = get_parameter('post_url');
+}
+
+Power.input = function* (state) {
+    yield* rt.http.post(post_url, (state)?'color=ffffff':'color=000000');
+}
+
