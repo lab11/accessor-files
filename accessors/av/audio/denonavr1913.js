a49e7d0fef788c8b01285c939469874c27d7248d
Pat Pannuto <pat.pannuto@gmail.com>
2015-05-20 12:49:36 -0400

    Rename denon ports for new style

--- accessors/av/audio/denonavr1913.js
+++ accessors/av/audio/denonavr1913.js
@@ -67,13 +67,13 @@ function* init () {
   // }
 }
 
-function* Power (on) {
+Power.input = function* (on) {
   var cmd_url = get_parameter('device_url') + '/MainZone/index.put.asp';
   yield* rt.http.request(cmd_url, 'POST', null, 'cmd0=PutZone_OnOff/'+(on?'ON':'OFF'), 3000);
   rt.log.debug(on);
 }
 
-function* Volume (volume) {
+Volume.input = function* (volume) {
 	var set_volume = parseFloat(volume) - 80;
 	var cmd_url = get_parameter('device_url') + '/MainZone/index.put.asp';
 	yield* rt.http.request(cmd_url, 'POST', null, 'cmd0=PutMasterVolumeSet/'+set_volume, 3000);
