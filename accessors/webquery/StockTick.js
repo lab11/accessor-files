71d5b61aa145c979600090c3ab5b10586ddd13cb
Brad Campbell <bradjc5@gmail.com>
2015-05-20 02:02:30 -0400

    fixed stock tick

--- accessors/webquery/StockTick.js
+++ accessors/webquery/StockTick.js
@@ -7,6 +7,8 @@
 // This accessor, when fired, reads the most recent bid price for the specified
 // stock symbol from a Yahoo server.
 
+var stock_symbol = 'YHOO';
+
 function init() {
   create_port('StockSymbol', {
     display_name: "Stock Symbol",
@@ -21,21 +23,19 @@ function init() {
 }
 
 StockSymbol.input = function* (symbol) {
-  rt.log.debug("StockTick StockSymbol start");
-  var stock = get('StockSymbol');
-  var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22"+ stock + "%22)%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json";
-  var record = yield* rt.http.readURL(url);
-  var json = JSON.parse(record);
-  if (json.query.results == null) {
-    rt.log.error("Stock query failed");
-    // set('Price', '<ERR>');
-  } else {
-    var tick = parseFloat(json.query.results.quote.LastTradePriceOnly);
-    // set('Price', tick);
-  }
-  rt.log.debug("StockTick StockSymbol end");
+  stock_symbol = symbol;
 }
 
 Price.output = function* () {
-
+  rt.log.debug("StockTick StockSymbol start");
+  var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22"+ stock_symbol + "%22)%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json";
+  var record = yield* rt.http.get(url);
+  var json = JSON.parse(record);
+  if (json.query.results == null) {
+    rt.log.error("Stock query failed");
+  } else {
+    var tick = parseFloat(json.query.results.quote.LastTradePriceOnly);
+    return tick;
+  }
+  rt.log.debug("StockTick StockSymbol end");
 }
