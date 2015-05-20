37f9207fe6adc94d266b8880f15c9855bc0085f5
William Huang <wwhuang@umich.edu>
2015-05-20 15:36:18 -0400

    mandatory comments??

--- accessors/sensor/willsensor.js
+++ accessors/sensor/willsensor.js
@@ -1,3 +1,31 @@
+// name:   4908 Lights
+// author: Will Huang
+// email: wwwhuang@umich.edu
+
+/* Light and Temperature Sensor 
+ * ======================
+ *
+ * If this doesn't work, it's brad's fault
+ */
+
+
+function* init () {
+	provide_interface('/lighting/light', {
+		'/onoff/Power': Power
+	});
+}
+
+Power.input = function* (state) {
+	var post_url = get_parameter('post_url');
+	var location = get_parameter('location_str');
+	var data = {};
+
+	data['light_command'] = (state) ? 'on' : 'off';
+	data['location_str'] = location;
+	yield* rt.http.request(post_url, 'POST', {'Content-Type': 'application/json'}, JSON.stringify(data), 0);
+}
+
+
 var ip_addr;
 
 function* init () {
