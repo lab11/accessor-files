52dda9417f3e5aa20bc73df6263d54117a4510d5
Brad Campbell <bradjc5@gmail.com>
2015-05-21 17:47:42 -0400

    need json stringify

--- accessors/webquery/Random.js
+++ accessors/webquery/Random.js
@@ -31,7 +31,7 @@ RandomInteger.output = function* () {
   };
 
   // var resp = yield* rt.http.request('https://api.random.org/json-rpc/1/invoke', 'GET', null, msg, 0);
-  var resp = yield* rt.http.post('https://api.random.org/json-rpc/1/invoke', msg);
+  var resp = yield* rt.http.post('https://api.random.org/json-rpc/1/invoke', JSON.stringify(msg));
   rt.log.log(resp);
 
   request_id += 1;
