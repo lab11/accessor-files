580d2982c964e6050d940cc9f1d9b925b5e5b843
Branden Ghena <brghena@umich.edu>
2015-06-20 19:31:22 -0400

    Lets see what accessors.io does with this

--- accessors/lighting/wemoLight.js
+++ accessors/lighting/wemoLight.js
@@ -23,6 +23,8 @@ var port;
 var id;
 
 function* init () {
+    rt.log.debug("Creating WeMo Light accessor!!");
+
 	// INTERFACES
 	provide_interface('/lighting/light');
 	provide_interface('/lighting/brightness');
@@ -49,6 +51,10 @@ function* get_light_state () {
 }
 
 function* set_light_state (capability_id, capability_value) {
+    rt.log.debug("Setting light state with : ");
+    rt.log.debug(capability_id);
+    rt.log.debug(capability_value);
+
     var url = ip_addr + ':' + port + '/upnp/control/bridge1';
     var headers = {
 	    'SOAPACTION': '"urn:Belkin:service:bridge:1#SetDeviceStatus"',
@@ -63,29 +69,31 @@ function* set_light_state (capability_id, capability_value) {
         '</u:SetDeviceStatus>',
         postbodyfooter
 	].join('\n');
-    body.replace('{deviceid}', id);
-    body.replace('{capabilityid}', capability_id);
-    body.replace('{capabilityvalue}', capability_value);
-    console.log.debug(url + '\n' + headers + '\n' + body);
-    console.log.debug("Posting...");
+    body = body.replace('{deviceid}', id);
+    body = body.replace('{capabilityid}', capability_id);
+    body = body.replace('{capabilityvalue}', capability_value);
+    rt.log.debug(url + '\n' + headers + '\n' + body);
+    rt.log.debug("Posting...");
     var response = yield* rt.http.request(url, 'POST', headers, body, 0);
-    console.log.debug("Response: ");
-    console.log.debug(response);
+    rt.log.debug("Response: ");
+    rt.log.debug(response);
 }
 
 lighting.light.Power.input = function* (state) {
-    set_light_state('10006', (state)?'1':'0');
+    rt.log.debug("Power input called");
+    yield* set_light_state('10006', (state)?'1':'0');
 }
 
 //XXX: Implement me!
 lighting.light.Power.output = function* () {
+    rt.log.debug("Power output called");
 	//var val = yield* rt.coap.get('coap://['+ip_addr+']/onoff/Power');
 	//return val == 'true';
     return 'true';
 }
 
 lighting.brightness.Brightness.input = function* (brightness) {
-    set_light_state('10008', brightness.toString()+":0");
+    yield* set_light_state('10008', brightness.toString()+":0");
 }
 
 //XXX: Implement me!
