404859fc0dc7f7556d8de5f6a4d02df1487dc43a
Brad Campbell <bradjc5@gmail.com>
2015-06-23 20:06:38 -0400

    scarab updates

--- accessors/robot/scarab.js
+++ accessors/robot/scarab.js
@@ -11,13 +11,13 @@
 var websocket = require('webSocket');
 
 function setup () {
-  createPort('X', ['write'], {
+  createPort('X', ['write', 'eventperiodic'], {
     type: 'numeric'
   });
-  createPort('Y', ['write'], {
+  createPort('Y', ['write', 'eventperiodic'], {
     type: 'numeric'
   });
-  createPort('Z', ['write'], {
+  createPort('Z', ['write', 'eventperiodic'], {
     type: 'numeric'
   });
   createPort('Position', ['write'], {
@@ -30,6 +30,10 @@ var ws;
 var connection_opened = false;
 var seq = 0;
 
+var subscriptions = [
+  '/scarab0/gt_pose',   // where the scarab is
+];
+
 
 function* init () {
 
@@ -40,16 +44,49 @@ function* init () {
   ws.on('open', function () {
     connection_opened = true;
     console.log('Connected to scarab');
+
+    // Subscribe to things we care about
+    for (var i=0; i<subscriptions.length; i++) {
+      var data = {
+        op: 'subscribe',
+        topic: subscriptions[i]
+      }
+      ws.send(JSON.stringify(data));
+    }
+
   });
+
   ws.on('error', function () {
     console.log('Err. Host: ws://' + getParameter('host'));
   });
+
   ws.on('message', function (data, flags) {
-    console.log('Got message');
+    // console.log('Got message ' + data);
+
+    var item = JSON.parse(data);
+    if ('topic' in item) {
+      if (item.topic == '/scarab0/gt_pose') {
+        var x = item.msg.pose.position.x;
+        var y = item.msg.pose.position.y;
+        var z = item.msg.pose.position.z;
+        var ox = item.msg.pose.orientation.x;
+        var oy = item.msg.pose.orientation.y;
+        var oz = item.msg.pose.orientation.z;
+
+        send('X', x);
+        send('Y', y);
+        send('Z', z);
+
+      } else {
+        console.log('Got unknown message ' + data);
+      }
+    }
+
   });
 
 }
 
+// Send a position to the robot
 position = function* (val) {
   var coord_x = parseFloat(val.x);
   var coord_y = parseFloat(val.y);
