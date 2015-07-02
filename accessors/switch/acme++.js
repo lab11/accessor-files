14c8c9146ff19ebc47c43022a735d6da0661966e
Brad Campbell <bradjc5@gmail.com>
2015-07-01 21:18:44 -0400

    update acme++ accessor to work

--- accessors/switch/acme++.js
+++ accessors/switch/acme++.js
@@ -25,16 +25,43 @@ function* init () {
 	addOutputHandler('/sensor/power.Power', outWatts);
 }
 
+function req_succedded (status_code) {
+	var sc = parseFloat(status_code);
+
+	if (sc >= 2.0 && sc < 3.0) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
 inPower = function* (state) {
 	yield* coap.post('coap://['+ip_addr+']/onoff/Power', (state)?'true':'false');
 }
 
 outPower = function* () {
-	var val = yield* coap.get('coap://['+ip_addr+']/onoff/Power');
+	var req = yield* coap.get('coap://['+ip_addr+']/onoff/Power');
+
+	if (!req_succedded(req.statusCode)) {
+		req = yield* coap.get('coap://['+ip_addr+']/onoffdevice/Power');
+		if (!req_succedded(req.statusCode)) {
+			throw 'Could not get power state.';
+		}
+	}
+
+	var val = req.body.toString('utf-8');
 	send('/onoff.Power', val == 'true');
 }
 
 outWatts = function* () {
-	var watt = yield* rt.coap.get('coap://['+ip_addr+']/powermeter/Power');
+	var req = yield* coap.get('coap://['+ip_addr+']/powermeter/Power');
+	if (!req_succedded(req.statusCode)) {
+		throw 'Could not get power.';
+	}
+
+	var watt = req.body.toString('utf-8');
+	if (watt.length > 6 && watt.substring(0, 6) == 'Power=') {
+		throw 'This old ACme++ does not support power.';
+	}
 	send('/sensor/power.Power', watt);
 }
